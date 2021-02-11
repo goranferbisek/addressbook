0292c1f3f5c149914d562f72d92b0326fd2791bc
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-02-11 20:08:50 +0100

    Return a JSON object

--- src/main/java/si/goranferbisek/addressbook/PersonResource.java
+++ src/main/java/si/goranferbisek/addressbook/PersonResource.java
@@ -1,20 +1,35 @@
 package si.goranferbisek.addressbook;
 
 import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-/**
- * Sample JAX-RS resources.
- *
- */
+
 @Path("person")
 @RequestScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PersonResource {
     
     @GET
-    public String getMessage() {
-        return "Hello, world";
+    public Response getMessage() {
+    	JsonObject json = Json.createObjectBuilder()
+    			.add("message", "The API is working")
+    			.build();
+    	
+        return Response.ok().entity(json).build();
+    }
+    
+    @POST
+    public Response savePerson() {
+    	return Response.ok().build();
     }
     
 }
