fee8390d75271015bfc16bb65c77b551e9eb468a
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-02-15 17:57:46 +0100

    Add contact database with foreign key

--- src/main/resources/addressbook_database.sql
+++ src/main/resources/addressbook_database.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE IF NOT EXISTS addressbook;
+DROP DATABASE IF EXISTS addressbook;
+
+CREATE DATABASE addressbook CHARSET = utf8 COLLATE = utf8_general_ci;
 USE addressbook;
 
 
@@ -6,7 +8,16 @@ CREATE TABLE person (
   id INT NOT NULL AUTO_INCREMENT,
   name varchar(128) NOT NULL,
   PRIMARY KEY (id)
-);
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE contact (
+  id INT NOT NULL AUTO_INCREMENT,
+  person_id INT NOT NULL,
+  phone varchar(128) NOT NULL,
+  email varchar(128) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_person_id FOREIGN KEY (person_id) REFERENCES person (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 
 
@@ -15,3 +26,8 @@ VALUES
     ("Janez Novak"),
     ("Nina Zupan"),
     ("Ana Petek");
+    
+INSERT INTO contact (person_id, phone, email)
+VALUES
+	(1, "+38641700700", "janez.novak@gmail.com"),
+	(2, "+38631999666", "nina.zupan@gmail.com");
